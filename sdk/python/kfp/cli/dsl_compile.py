6e59eb5603cfa781af3e0370a648b00a43f24f40
connor-mccarthy <mccarthy.connor.james@gmail.com>
2022-05-03 10:08:31 -0600

    extract helper functions from dsl_compile

--- sdk/python/kfp/cli/dsl_compile.py
+++ sdk/python/kfp/cli/dsl_compile.py
@@ -18,15 +18,16 @@ import logging
 import os
 import sys
 import types
-from typing import Any, Callable, List, Optional
+from typing import Any, Callable, Dict, Optional, Union
 
 import click
 from kfp import compiler
 from kfp.components import pipeline_context
+from kfp.components import python_component
 
 
-def collect_pipeline_from_module(
-        target_module: types.ModuleType) -> List[Callable[..., Any]]:
+def collect_default_pipeline_from_module(
+        target_module: types.ModuleType) -> Callable[..., Any]:
     pipelines = []
     module_attrs = dir(target_module)
     for attr in module_attrs:
@@ -39,6 +40,38 @@ def collect_pipeline_from_module(
         raise ValueError(
             f'Expect one pipeline function in module {target_module}, got {len(pipelines)}: {pipelines}. Please specify the pipeline function name with --function.'
         )
+
+
+def collect_pipeline_func(
+    python_file: str, function_name: Optional[str]
+) -> Union[Callable[..., Any], python_component.PythonComponent]:
+    sys.path.insert(0, os.path.dirname(python_file))
+    try:
+        filename = os.path.basename(python_file)
+        module_name = os.path.splitext(filename)[0]
+        if function_name is None:
+            return collect_default_pipeline_from_module(
+                target_module=__import__(module_name))
+
+        module = __import__(module_name, fromlist=[function_name])
+        if not hasattr(module, function_name):
+            raise ValueError(
+                f'Pipeline function or component "{function_name}" not found in module {filename}.'
+            )
+
+        return getattr(module, function_name)
+
+    finally:
+        del sys.path[0]
+
+
+def parse_parameters(parameters: Optional[str]) -> Dict:
+    try:
+        return json.loads(parameters) if parameters is not None else {}
+    except json.JSONDecodeError as e:
+        logging.error(
+            f"Failed to parse --pipeline-parameters argument: {parameters}")
+        raise e
 
 
 @click.command()
@@ -69,40 +102,19 @@ def dsl_compile(
     py: str,
     output: str,
     function_name: Optional[str] = None,
-    pipeline_parameters: str = None,
+    pipeline_parameters: Optional[str] = None,
     disable_type_check: bool = True,
 ) -> None:
     """Compiles a pipeline written in a .py file."""
-    sys.path.insert(0, os.path.dirname(py))
-    try:
-        filename = os.path.basename(py)
-        module_name = os.path.splitext(filename)[0]
-        if function_name:
-            module = __import__(module_name, fromlist=[function_name])
-            if not hasattr(module, function_name):
-                raise ValueError(
-                    f'Pipeline function or component "{function_name}" not found in module {filename}.'
-                )
-            pipeline_func = getattr(module, function_name)
-        else:
-            pipeline_func = collect_pipeline_from_module(
-                target_module=__import__(module_name))
+    pipeline_func = collect_pipeline_func(
+        python_file=py, function_name=function_name)
+    parsed_parameters = parse_parameters(parameters=pipeline_parameters)
 
-        try:
-            parsed_parameters = json.loads(
-                pipeline_parameters) if pipeline_parameters is not None else {}
-        except json.JSONDecodeError as e:
-            logging.error(
-                f"Failed to parse --pipeline-parameters argument: {pipeline_parameters}"
-            )
-            raise e
-        compiler.Compiler().compile(
-            pipeline_func=pipeline_func,
-            pipeline_parameters=parsed_parameters,
-            package_path=output,
-            type_check=not disable_type_check)
-    finally:
-        del sys.path[0]
+    compiler.Compiler().compile(
+        pipeline_func=pipeline_func,
+        pipeline_parameters=parsed_parameters,
+        package_path=output,
+        type_check=not disable_type_check)
 
 
 def main():
