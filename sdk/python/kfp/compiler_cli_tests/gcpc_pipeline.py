16c65f8340b7b4592e196e3c6070bebcae1d2dbd
connor-mccarthy <mccarthy.connor.james@gmail.com>
2022-06-07 14:49:26 -0600

    add sample test

--- sdk/python/kfp/compiler_cli_tests/gcpc_pipeline.py
+++ sdk/python/kfp/compiler_cli_tests/gcpc_pipeline.py
@@ -1,22 +1,25 @@
-import google.cloud.aiplatform as aip
-from google_cloud_pipeline_components import aiplatform as gcc_aip
-import kfp
-from kfp.v2 import dsl
-from kfp.v2.dsl import Input
+import os
 
-DISPLAY_NAME = 'test-vertex-dataset'
-PIPELINE_NAME = 'test-vertex-dataset'
-PROJECT_ID = 'cjmccarthy-kfp'
+from google_cloud_pipeline_components.types import artifact_types
+from kfp import components
+from kfp import dsl
+from kfp.components import importer_node
+from kfp.dsl import Input
 
-from google_cloud_pipeline_components.types.artifact_types import VertexDataset
-from kfp.v2.dsl import Input
+project_root = os.path.abspath(os.path.join(__file__, *([os.path.pardir] * 5)))
+component_path = os.path.join(project_root, 'components', 'google-cloud',
+                              'google_cloud_pipeline_components', 'v1', 'model',
+                              'upload_model', 'component.yaml')
+gcpc_component = components.load_component_from_file(component_path)
 
 
 @dsl.component(
-    kfp_package_path='git+https://github.com/connor-mccarthy/pipelines@connor-custom-type#egg=kfp&subdirectory=sdk/python',
-    packages_to_install=['google_cloud_pipeline_components'],
+    kfp_package_path='git+https://github.com/connor-mccarthy/pipelines@support-custom-artifact-types#subdirectory=sdk/python',
+    packages_to_install=[
+        'git+https://github.com/connor-mccarthy/pipelines@support-custom-artifact-types#subdirectory=components/google-cloud'
+    ],
 )
-def dummy_op(artifact: Input[VertexDataset]):
+def dummy_op(artifact: Input[artifact_types.UnmanagedContainerModel]):
     print('artifact.type: ', type(artifact))
     print('artifact.name: ', artifact.name)
     print('artifact.uri: ', artifact.uri)
@@ -24,23 +27,24 @@ def dummy_op(artifact: Input[VertexDataset]):
 
 
 @dsl.pipeline(
-    name=PIPELINE_NAME, pipeline_root='gs://cjmccarthy-kfp-default-bucket')
-def pipeline():
-    ds_op = gcc_aip.ImageDatasetCreateOp(
-        project=PROJECT_ID,
-        display_name=DISPLAY_NAME,
-        gcs_source='gs://cloud-samples-data/vision/automl_classification/flowers/all_data_v2.csv',
-        import_schema_uri=aip.schema.dataset.ioformat.image
-        .single_label_classification,
-    )
+    name='test-custom-artifact',
+    pipeline_root='gs://cjmccarthy-kfp-default-bucket')
+def my_pipeline():
+    importer_spec = importer_node.importer(
+        artifact_uri='gs://managed-pipeline-gcpc-e2e-test/automl-tabular/model',
+        artifact_class=artifact_types.UnmanagedContainerModel,
+        metadata={
+            'containerSpec': {
+                'imageUri':
+                    'us-docker.pkg.dev/vertex-ai/automl-tabular/prediction-server:prod'
+            }
+        })
 
-    dummy_op(ds_op.outputs['dataset'])
+    dummy_op(artifact=importer_spec.outputs['artifact'])
 
 
 if __name__ == '__main__':
-    import json
-
-    from kfp.v2 import compiler
-    import yaml
-    ir_file = __file__.replace('.py', '.json')
-    compiler.Compiler().compile(pipeline_func=pipeline, package_path=ir_file)
+    from kfp import compiler
+    ir_file = __file__.replace('.py', '.yaml')
+    compiler.Compiler().compile(
+        pipeline_func=my_pipeline, package_path=ir_file, type_check=False)
