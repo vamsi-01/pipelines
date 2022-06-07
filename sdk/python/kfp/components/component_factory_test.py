de97d2e9257dac2c2a2db58caa6ec49be2eb8edb
connor-mccarthy <mccarthy.connor.james@gmail.com>
2022-06-07 14:46:21 -0600

    add tests

--- sdk/python/kfp/components/component_factory_test.py
+++ sdk/python/kfp/components/component_factory_test.py
@@ -12,9 +12,21 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import inspect
+import os
+import sys
+import tempfile
+import textwrap
+from typing import Any, Dict
 import unittest
 
+from absl.testing import parameterized
 from kfp.components import component_factory
+from kfp.components.types import artifact_types
+from kfp.components.types.artifact_types import Artifact
+from kfp.components.types.artifact_types import Dataset
+from kfp.components.types.type_annotations import Input
+from kfp.components.types.type_annotations import Output
 
 
 class TestGetPackagesToInstallCommand(unittest.TestCase):
@@ -44,3 +56,340 @@ class TestGetPackagesToInstallCommand(unittest.TestCase):
         concat_command = ' '.join(command)
         for package in packages_to_install + pip_index_urls:
             self.assertTrue(package in concat_command)
+
+
+Alias = Artifact
+artifact_types_alias = artifact_types
+
+
+class TestGetSignatureFromFunc(unittest.TestCase):
+
+    def test_no_annotations(self):
+
+        def func(a, b):
+            pass
+
+        actual = component_factory.get_signature_from_func(func)
+        expected = 'def func(a, b):'
+        self.assertEqual(actual, expected)
+
+    def test_no_return(self):
+
+        def func(a: int, b: Input[Artifact]):
+            pass
+
+        actual = component_factory.get_signature_from_func(func)
+        expected = 'def func(a: int, b: Input[Artifact]):'
+        self.assertEqual(actual, expected)
+
+    def test_with_return(self):
+
+        def func(a: int, b: Input[Artifact]) -> int:
+            return 1
+
+        actual = component_factory.get_signature_from_func(func)
+        expected = 'def func(a: int, b: Input[Artifact]) -> int:'
+        self.assertEqual(actual, expected)
+
+    def test_multiline(self):
+
+        def func(
+            a: int,
+            b: Input[Artifact],
+        ) -> int:
+            return 1
+
+        actual = component_factory.get_signature_from_func(func)
+        expected = 'def func(\n    a: int,\n    b: Input[Artifact],\n) -> int:'
+        self.assertEqual(actual, expected)
+
+    def test_alias(self):
+
+        def func(a: int, b: Input[Alias]):
+            pass
+
+        actual = component_factory.get_signature_from_func(func)
+        expected = 'def func(a: int, b: Input[Alias]):'
+        self.assertEqual(actual, expected)
+
+    def test_long_form_annotation(self):
+
+        def func(a: int, b: Input[artifact_types.Artifact]):
+            pass
+
+        actual = component_factory.get_signature_from_func(func)
+        expected = 'def func(a: int, b: Input[artifact_types.Artifact]):'
+        self.assertEqual(actual, expected)
+
+
+class TestGetParamToAnnStringFromSignature(parameterized.TestCase):
+
+    @parameterized.parameters([
+        ('def func(a, b):', {
+            'a': None,
+            'b': None
+        }),
+        ('def func(a: int, b: Input[Artifact]):', {
+            'a': 'int',
+            'b': 'Input[Artifact]'
+        }),
+        ('def func(a: int, b: Input[Artifact]) -> int:', {
+            'a': 'int',
+            'b': 'Input[Artifact]',
+            'return': 'int'
+        }),
+        ('def func(\n    a: int,\n    b: Input[Artifact],\n) -> int:', {
+            'a': 'int',
+            'b': 'Input[Artifact]',
+            'return': 'int'
+        }),
+        ('def func(a: int, b: Input[Alias]):', {
+            'a': 'int',
+            'b': 'Input[Alias]'
+        }),
+        ('def func(a: int, b: Input[artifact_types.Artifact]):', {
+            'a': 'int',
+            'b': 'Input[artifact_types.Artifact]'
+        }),
+    ])
+    def test(self, signature: str, expected: Dict[str, str]):
+        self.assertEqual(
+            component_factory.get_param_to_ann_string_from_signature(signature),
+            expected)
+
+
+class TestGetParamToAnnString(unittest.TestCase):
+
+    def test_no_annotations(self):
+
+        def func(a, b):
+            pass
+
+        actual = component_factory.get_param_to_ann_string(func)
+        expected = {'a': None, 'b': None}
+        self.assertEqual(actual, expected)
+
+    def test_no_return(self):
+
+        def func(a: int, b: Input[Artifact]):
+            pass
+
+        actual = component_factory.get_param_to_ann_string(func)
+        expected = {'a': 'int', 'b': 'Input[Artifact]'}
+        self.assertEqual(actual, expected)
+
+    def test_with_return(self):
+
+        def func(a: int, b: Input[Artifact]) -> int:
+            return 1
+
+        actual = component_factory.get_param_to_ann_string(func)
+        expected = {'a': 'int', 'b': 'Input[Artifact]', 'return': 'int'}
+        self.assertEqual(actual, expected)
+
+    def test_multiline(self):
+
+        def func(
+            a: int,
+            b: Input[Artifact],
+        ) -> int:
+            return 1
+
+        actual = component_factory.get_param_to_ann_string(func)
+        expected = {'a': 'int', 'b': 'Input[Artifact]', 'return': 'int'}
+        self.assertEqual(actual, expected)
+
+    def test_alias(self):
+
+        def func(a: int, b: Input[Alias]):
+            pass
+
+        actual = component_factory.get_param_to_ann_string(func)
+        expected = {'a': 'int', 'b': 'Input[Alias]'}
+        self.assertEqual(actual, expected)
+
+    def test_long_form_annotation(self):
+
+        def func(a: int, b: Input[artifact_types.Artifact]):
+            pass
+
+        actual = component_factory.get_param_to_ann_string(func)
+        expected = {'a': 'int', 'b': 'Input[artifact_types.Artifact]'}
+        self.assertEqual(actual, expected)
+
+
+class MyCustomArtifact:
+    TYPE_NAME = 'my_custom_artifact'
+
+
+class _TestCaseWithThirdPartyPackage(parameterized.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+
+        class ThirdPartyArtifact:
+            TYPE_NAME = 'my_third_party_artifact'
+
+        class_source = textwrap.dedent(inspect.getsource(ThirdPartyArtifact))
+
+        tmp_dir = tempfile.TemporaryDirectory()
+        with open(os.path.join(tmp_dir.name, 'my_package.py'), 'w') as f:
+            f.write(class_source)
+        sys.path.append(tmp_dir.name)
+        cls.tmp_dir = tmp_dir
+
+    @classmethod
+    def teardownClass(cls):
+        sys.path.pop()
+        cls.tmp_dir.cleanup()
+
+
+class TestGetFullQualnameForClass(_TestCaseWithThirdPartyPackage):
+
+    @parameterized.parameters([
+        (Alias, 'kfp.components.types.artifact_types.Artifact'),
+        (Artifact, 'kfp.components.types.artifact_types.Artifact'),
+        (Dataset, 'kfp.components.types.artifact_types.Dataset'),
+    ])
+    def test(self, obj: Any, expected_qualname: str):
+        self.assertEqual(
+            component_factory.get_full_qualname_for_annotation(obj),
+            expected_qualname)
+
+    def test_my_package_artifact(self):
+        import my_package
+        self.assertEqual(
+            component_factory.get_full_qualname_for_annotation(
+                my_package.ThirdPartyArtifact), 'my_package.ThirdPartyArtifact')
+
+
+class TestIsArtifact(_TestCaseWithThirdPartyPackage):
+
+    @parameterized.parameters([
+        (Dataset, True),
+        (Artifact, True),
+        (Alias, True),
+        (MyCustomArtifact, True),
+        (list, False),
+    ])
+    def test(self, obj: Any, expected: bool):
+        self.assertEqual(component_factory.is_artifact(obj), expected)
+
+    def test_my_package_artifact(self):
+        import my_package
+        self.assertEqual(
+            component_factory.is_artifact(my_package.ThirdPartyArtifact), True)
+
+
+class GetArtifactImportItemsFromFunction(_TestCaseWithThirdPartyPackage):
+
+    def test_no_annotations(self):
+
+        def func(a, b):
+            pass
+
+        actual = component_factory.get_artifact_import_items_from_function(func)
+        expected = []
+        self.assertEqual(actual, expected)
+
+    def test_no_return(self):
+        from my_package import ThirdPartyArtifact
+
+        def func(a: int, b: Input[ThirdPartyArtifact]):
+            pass
+
+        actual = component_factory.get_artifact_import_items_from_function(func)
+        expected = [{
+            'name': 'my_package.ThirdPartyArtifact',
+            'alias': 'ThirdPartyArtifact'
+        }]
+        self.assertEqual(actual, expected)
+
+    def test_with_return(self):
+        from my_package import ThirdPartyArtifact
+
+        def func(a: int, b: Input[ThirdPartyArtifact]) -> int:
+            return 1
+
+        actual = component_factory.get_artifact_import_items_from_function(func)
+        expected = [{
+            'name': 'my_package.ThirdPartyArtifact',
+            'alias': 'ThirdPartyArtifact'
+        }]
+        self.assertEqual(actual, expected)
+
+    def test_multiline(self):
+        from my_package import ThirdPartyArtifact
+
+        def func(
+            a: int,
+            b: Input[ThirdPartyArtifact],
+        ) -> int:
+            return 1
+
+        actual = component_factory.get_artifact_import_items_from_function(func)
+        expected = [{
+            'name': 'my_package.ThirdPartyArtifact',
+            'alias': 'ThirdPartyArtifact'
+        }]
+        self.assertEqual(actual, expected)
+
+    def test_alias(self):
+        from my_package import ThirdPartyArtifact
+        Alias = ThirdPartyArtifact
+
+        def func(a: int, b: Input[Alias]):
+            pass
+
+        actual = component_factory.get_artifact_import_items_from_function(func)
+        expected = [{'name': 'my_package.ThirdPartyArtifact', 'alias': 'Alias'}]
+        self.assertEqual(actual, expected)
+
+    def test_long_form_annotation(self):
+        import my_package
+
+        def func(a: int, b: Output[my_package.ThirdPartyArtifact]):
+            pass
+
+        actual = component_factory.get_artifact_import_items_from_function(func)
+        expected = [{'name': 'my_package', 'alias': 'my_package'}]
+        self.assertEqual(actual, expected)
+
+    def test_aliased_module_throws_error(self):
+        import my_package as my_package_alias
+
+        def func(a: int, b: Output[my_package_alias.ThirdPartyArtifact]):
+            pass
+
+        with self.assertRaisesRegex(
+                TypeError,
+                'due to an aliased module used in the type annotation'):
+            component_factory.get_artifact_import_items_from_function(func)
+
+
+class TestImportArtifactAnnotations(_TestCaseWithThirdPartyPackage):
+
+    def test(self):
+        import_items = [
+            {
+                'name': 'kfp.components.types.artifact_types.Dataset',
+                'alias': 'my_alias'
+            },
+            {
+                'name': 'typing.TypeVar',
+                'alias': 'type_var_alias'
+            },
+        ]
+        aliases = [item['alias'] for item in import_items]
+
+        for alias in aliases:
+            self.assertNotIn(alias, globals())
+
+        component_factory.import_artifact_annotations(import_items, globals())
+
+        for alias in aliases:
+            self.assertIn(alias, globals())
+
+
+if __name__ == '__main__':
+    unittest.main()
