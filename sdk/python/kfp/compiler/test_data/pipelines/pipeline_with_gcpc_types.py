6b47db04db8e79e08b3bfc752bf65fa618bd0e6d
connor-mccarthy <mccarthy.connor.james@gmail.com>
2022-06-16 08:55:03 -0600

    add compiler read/write test case for gcpc types

--- sdk/python/kfp/compiler/test_data/pipelines/pipeline_with_gcpc_types.py
+++ sdk/python/kfp/compiler/test_data/pipelines/pipeline_with_gcpc_types.py
@@ -1,53 +1,62 @@
-# Copyright 2021 The Kubeflow Authors
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+import os
+from typing import NamedTuple
 
+from google_cloud_pipeline_components.types import artifact_types
+from google_cloud_pipeline_components.types.artifact_types import \
+    UnmanagedContainerModel
 from kfp import compiler
 from kfp import components
 from kfp import dsl
-from kfp.dsl import component
+from kfp.components import importer_node
 from kfp.dsl import Input
 
-
-class VertexModel(dsl.Artifact):
-    TYPE_NAME = 'google.VertexModel'
-
-
-producer_op = components.load_component_from_text("""
-name: producer
-outputs:
-  - {name: model, type: google.VertexModel}
-implementation:
-  container:
-    image: dummy
-    command:
-    - cmd
-    args:
-    - {outputPath: model}
-""")
+project_root = os.path.abspath(os.path.join(__file__, *([os.path.pardir] * 7)))
+component_path = os.path.join(project_root, 'components', 'google-cloud',
+                              'google_cloud_pipeline_components', 'v1', 'model',
+                              'upload_model', 'component.yaml')
+gcpc_component = components.load_component_from_file(component_path)
 
 
-@component
-def consumer_op(model: Input[VertexModel]):
-    pass
+@dsl.component
+def dummy_op(
+    artifact: Input[artifact_types.UnmanagedContainerModel]
+) -> artifact_types.UnmanagedContainerModel:
+    print('artifact.type: ', type(artifact))
+    print('artifact.name: ', artifact.name)
+    print('artifact.uri: ', artifact.uri)
+    print('artifact.metadata: ', artifact.metadata)
+    return artifact
+
+
+@dsl.component
+def named_tuple_op(
+    artifact: Input[UnmanagedContainerModel]
+) -> NamedTuple('NamedTupleOutput',
+                [('model1', artifact_types.UnmanagedContainerModel),
+                 ('model2', artifact_types.UnmanagedContainerModel)]):
+    NamedTupleOutput = NamedTuple(
+        'NamedTupleOutput',
+        [('model1', artifact_types.UnmanagedContainerModel),
+         ('model2', artifact_types.UnmanagedContainerModel)])
+    return NamedTupleOutput(model1=artifact, model2=artifact)
 
 
 @dsl.pipeline(name='pipeline-with-gcpc-types')
 def my_pipeline():
-    consumer_op(model=producer_op().outputs['model'])
+    importer_spec = importer_node.importer(
+        artifact_uri='gs://managed-pipeline-gcpc-e2e-test/automl-tabular/model',
+        artifact_class=artifact_types.UnmanagedContainerModel,
+        metadata={
+            'containerSpec': {
+                'imageUri':
+                    'us-docker.pkg.dev/vertex-ai/automl-tabular/prediction-server:prod'
+            }
+        })
+    dummy_task = dummy_op(artifact=importer_spec.outputs['artifact'])
+
+    named_tuple_op(artifact=dummy_task.output)
 
 
 if __name__ == '__main__':
-    compiler.Compiler().compile(
-        pipeline_func=my_pipeline,
-        package_path=__file__.replace('.py', '.yaml'))
+    ir_file = __file__.replace('.py', '.yaml')
+    compiler.Compiler().compile(pipeline_func=my_pipeline, package_path=ir_file)
