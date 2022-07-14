7ceed067d0e4073ff8a0bf8ad11af0c2bbeca2b3
connor-mccarthy <mccarthy.connor.james@gmail.com>
2022-07-14 08:25:22 -0600

    use __all__ in top-level modules to record public api

--- sdk/python/kfp/dsl/__init__.py
+++ sdk/python/kfp/dsl/__init__.py
@@ -12,39 +12,52 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+__all__ = [
+    'component',
+    'importer',
+    'PipelineArtifactChannel',
+    'PipelineChannel',
+    'PipelineParameterChannel',
+    'pipeline',
+    'PipelineTask',
+    'PipelineTaskFinalStatus',
+    'Condition',
+    'ExitHandler',
+    'ParallelFor',
+    'Artifact',
+    'ClassificationMetrics',
+    'Dataset',
+    'HTML',
+    'Markdown',
+    'Metrics',
+    'Model',
+    'SlicedClassificationMetrics',
+    'Input',
+    'Output',
+    'InputPath',
+    'OutputPath',
+    'PIPELINE_JOB_NAME_PLACEHOLDER',
+    'PIPELINE_JOB_RESOURCE_NAME_PLACEHOLDER',
+    'PIPELINE_JOB_ID_PLACEHOLDER',
+    'PIPELINE_TASK_NAME_PLACEHOLDER',
+    'PIPELINE_TASK_ID_PLACEHOLDER',
+]
+
 from kfp.components.component_decorator import component
-
 from kfp.components.importer_node import importer
-
-from kfp.components.pipeline_channel import (
-    PipelineArtifactChannel,
-    PipelineChannel,
-    PipelineParameterChannel,
-)
+from kfp.components.pipeline_channel import (PipelineArtifactChannel,
+                                             PipelineChannel,
+                                             PipelineParameterChannel)
 from kfp.components.pipeline_context import pipeline
 from kfp.components.pipeline_task import PipelineTask
 from kfp.components.task_final_status import PipelineTaskFinalStatus
-from kfp.components.tasks_group import (
-    Condition,
-    ExitHandler,
-    ParallelFor,
-)
-from kfp.components.types.artifact_types import (
-    Artifact,
-    ClassificationMetrics,
-    Dataset,
-    HTML,
-    Markdown,
-    Metrics,
-    Model,
-    SlicedClassificationMetrics,
-)
-from kfp.components.types.type_annotations import (
-    Input,
-    Output,
-    InputPath,
-    OutputPath,
-)
+from kfp.components.tasks_group import Condition, ExitHandler, ParallelFor
+from kfp.components.types.artifact_types import (HTML, Artifact,
+                                                 ClassificationMetrics, Dataset,
+                                                 Markdown, Metrics, Model,
+                                                 SlicedClassificationMetrics)
+from kfp.components.types.type_annotations import (Input, InputPath, Output,
+                                                   OutputPath)
 
 PIPELINE_JOB_NAME_PLACEHOLDER = '{{$.pipeline_job_name}}'
 PIPELINE_JOB_RESOURCE_NAME_PLACEHOLDER = '{{$.pipeline_job_resource_name}}'
--- sdk/python/kfp/registry/__init__.py
+++ sdk/python/kfp/registry/__init__.py
@@ -12,5 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from kfp.registry.registry_client import ApiAuth
-from kfp.registry.registry_client import RegistryClient
+__all__ = [
+    'ApiAuth',
+    'RegistryClient',
+]
+from kfp.registry.registry_client import ApiAuth, RegistryClient
