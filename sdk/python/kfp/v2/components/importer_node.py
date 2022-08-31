082d5c29a6b88edddc27c9c1e9782bbc2e026f71
connor-mccarthy <mccarthy.connor.james@gmail.com>
2022-08-31 15:20:06 -0600

    add support for dynamic importer metadata

--- sdk/python/kfp/v2/components/importer_node.py
+++ sdk/python/kfp/v2/components/importer_node.py
@@ -13,8 +13,10 @@
 # limitations under the License.
 """Utility function for building Importer Node spec."""
 
-from typing import Any, Union, Optional, Type, Mapping
+from typing import Any, Tuple, Union, Optional, Type, Mapping, Dict, List
 from google.protobuf import struct_pb2
+import sys
+import copy
 
 from kfp.dsl import _container_op
 from kfp.dsl import _pipeline_param
@@ -22,14 +24,69 @@ from kfp.dsl import dsl_utils
 from kfp.pipeline_spec import pipeline_spec_pb2
 from kfp.v2.components.types import artifact_types, type_utils
 
-INPUT_KEY = 'uri'
-OUTPUT_KEY = 'artifact'
+URI_INPUT_KEY = 'uri'
+URI_OUTPUT_KEY = 'artifact'
+
+
+def make_input_parameter_placeholder(key: str) -> str:
+    return f"{{{{$.inputs.parameters['{key}']}}}}"
+
+
+def make_placeholder_unique(
+    name: str,
+    collection: List[str],
+    delimiter: str,
+) -> str:
+    """Makes a unique name by adding index."""
+    unique_name = name
+    if unique_name in collection:
+        for i in range(2, sys.maxsize**10):
+            unique_name = name + delimiter + str(i)
+            if unique_name not in collection:
+                break
+    return unique_name
+
+
+def transform_metadata_and_get_inputs(
+    metadata: Dict[Union[str, _pipeline_param.PipelineParam],
+                   Union[_pipeline_param.PipelineParam, Any]]
+) -> Tuple[Dict[str, Any], List[_pipeline_param.PipelineParam]]:
+    inputs: List[_pipeline_param.PipelineParam] = []
+    input_keys: List[str] = []
+
+    def traverse_dict_and_create_metadata_inputs(d: Any) -> Any:
+        if isinstance(d, _pipeline_param.PipelineParam):
+            unique_name = make_placeholder_unique(
+                d.name,
+                input_keys,
+                '-',
+            )
+            new_pipeline_param = copy.deepcopy(d)
+            new_pipeline_param.name = unique_name
+            input_keys.append(unique_name)
+            inputs.append(new_pipeline_param)
+
+            return make_input_parameter_placeholder(unique_name)
+        elif isinstance(d, dict):
+            return {
+                traverse_dict_and_create_metadata_inputs(k):
+                traverse_dict_and_create_metadata_inputs(v)
+                for k, v in d.items()
+            }
+
+        elif isinstance(d, list):
+            return [traverse_dict_and_create_metadata_inputs(el) for el in d]
+        else:
+            return d
+
+    new_metadata = traverse_dict_and_create_metadata_inputs(metadata)
+    return new_metadata, inputs
 
 
 def _build_importer_spec(
     artifact_uri: Union[_pipeline_param.PipelineParam, str],
     artifact_type_schema: pipeline_spec_pb2.ArtifactTypeSchema,
-    metadata: Optional[Mapping[str, Any]] = None,
+    metadata_with_placeholders: Optional[Mapping[str, Any]] = None,
 ) -> pipeline_spec_pb2.PipelineDeploymentConfig.ImporterSpec:
     """Builds an importer executor spec.
 
@@ -37,21 +94,23 @@ def _build_importer_spec(
       artifact_uri: The artifact uri to import from.
       artifact_type_schema: The user specified artifact type schema of the
         artifact to be imported.
+      metadata_with_placeholders: Metadata dictionary with pipeline parameters replaced with string placeholders.
 
     Returns:
       An importer spec.
     """
+
     importer_spec = pipeline_spec_pb2.PipelineDeploymentConfig.ImporterSpec()
     importer_spec.type_schema.CopyFrom(artifact_type_schema)
 
     if isinstance(artifact_uri, _pipeline_param.PipelineParam):
-        importer_spec.artifact_uri.runtime_parameter = INPUT_KEY
+        importer_spec.artifact_uri.runtime_parameter = URI_INPUT_KEY
     elif isinstance(artifact_uri, str):
         importer_spec.artifact_uri.constant_value.string_value = artifact_uri
 
-    if metadata:
+    if metadata_with_placeholders:
         metadata_protobuf_struct = struct_pb2.Struct()
-        metadata_protobuf_struct.update(metadata)
+        metadata_protobuf_struct.update(metadata_with_placeholders)
         importer_spec.metadata.CopyFrom(metadata_protobuf_struct)
 
     return importer_spec
@@ -59,13 +118,13 @@ def _build_importer_spec(
 
 def _build_importer_task_spec(
     importer_base_name: str,
-    artifact_uri: Union[_pipeline_param.PipelineParam, str],
+    inputs: List[_pipeline_param.PipelineParam],
 ) -> pipeline_spec_pb2.PipelineTaskSpec:
     """Builds an importer task spec.
 
     Args:
       importer_base_name: The base name of the importer node.
-      artifact_uri: The artifact uri to import from.
+      inputs: Task inputs.
 
     Returns:
       An importer node task spec.
@@ -74,27 +133,24 @@ def _build_importer_task_spec(
     result.component_ref.name = dsl_utils.sanitize_component_name(
         importer_base_name)
 
-    if isinstance(artifact_uri, _pipeline_param.PipelineParam):
-        param = artifact_uri
+    for param in inputs:
         if param.op_name:
             result.inputs.parameters[
-                INPUT_KEY].task_output_parameter.producer_task = (
+                param.name].task_output_parameter.producer_task = (
                     dsl_utils.sanitize_task_name(param.op_name))
             result.inputs.parameters[
-                INPUT_KEY].task_output_parameter.output_parameter_key = param.name
+                param
+                .name].task_output_parameter.output_parameter_key = param.name
         else:
             result.inputs.parameters[
-                INPUT_KEY].component_input_parameter = param.full_name
-    elif isinstance(artifact_uri, str):
-        result.inputs.parameters[
-            INPUT_KEY].runtime_value.constant_value.string_value = artifact_uri
-
+                param.name].component_input_parameter = param.full_name
     return result
 
 
 def _build_importer_component_spec(
     importer_base_name: str,
     artifact_type_schema: pipeline_spec_pb2.ArtifactTypeSchema,
+    inputs: List[_pipeline_param.PipelineParam],
 ) -> pipeline_spec_pb2.ComponentSpec:
     """Builds an importer component spec.
 
@@ -102,6 +158,7 @@ def _build_importer_component_spec(
       importer_base_name: The base name of the importer node.
       artifact_type_schema: The user specified artifact type schema of the
         artifact to be imported.
+      inputs: Task inputs.
 
     Returns:
       An importer node component spec.
@@ -109,18 +166,23 @@ def _build_importer_component_spec(
     result = pipeline_spec_pb2.ComponentSpec()
     result.executor_label = dsl_utils.sanitize_executor_label(
         importer_base_name)
-    result.input_definitions.parameters[
-        INPUT_KEY].type = pipeline_spec_pb2.PrimitiveType.STRING
-    result.output_definitions.artifacts[OUTPUT_KEY].artifact_type.CopyFrom(
+    for param in inputs:
+        result.input_definitions.parameters[
+            param.name].type = type_utils._PARAMETER_TYPES_MAPPING.get(
+                param.param_type.lower())
+
+    result.output_definitions.artifacts[URI_OUTPUT_KEY].artifact_type.CopyFrom(
         artifact_type_schema)
 
     return result
 
 
-def importer(artifact_uri: Union[_pipeline_param.PipelineParam, str],
-             artifact_class: Type[artifact_types.Artifact],
-             reimport: bool = False,
-             metadata: Optional[Mapping[str, Any]] = None) -> _container_op.ContainerOp:
+def importer(
+        artifact_uri: Union[_pipeline_param.PipelineParam, str],
+        artifact_class: Type[artifact_types.Artifact],
+        reimport: bool = False,
+        metadata: Optional[Mapping[str,
+                                   Any]] = None) -> _container_op.ContainerOp:
     """dsl.importer for importing an existing artifact. Only for v2 pipeline.
 
     Args:
@@ -138,10 +200,11 @@ def importer(artifact_uri: Union[_pipeline_param.PipelineParam, str],
     """
 
     if isinstance(artifact_uri, _pipeline_param.PipelineParam):
-        input_param = artifact_uri
+        input_uri_param = copy.deepcopy(artifact_uri)
+        input_uri_param.name = URI_INPUT_KEY
     elif isinstance(artifact_uri, str):
-        input_param = _pipeline_param.PipelineParam(
-            name='uri', value=artifact_uri, param_type='String')
+        input_uri_param = _pipeline_param.PipelineParam(
+            name=URI_INPUT_KEY, value=artifact_uri, param_type='String')
     else:
         raise ValueError(
             'Importer got unexpected artifact_uri: {} of type: {}.'.format(
@@ -149,24 +212,32 @@ def importer(artifact_uri: Union[_pipeline_param.PipelineParam, str],
 
     old_warn_value = _container_op.ContainerOp._DISABLE_REUSABLE_COMPONENT_WARNING
     _container_op.ContainerOp._DISABLE_REUSABLE_COMPONENT_WARNING = True
-
     task = _container_op.ContainerOp(
         name='importer',
         image='importer_image',  # TODO: need a v1 implementation of importer.
+        arguments=[],
         file_outputs={
-            OUTPUT_KEY:
-                "{{{{$.outputs.artifacts['{}'].uri}}}}".format(OUTPUT_KEY)
+            URI_OUTPUT_KEY:
+                "{{{{$.outputs.artifacts['{}'].uri}}}}".format(URI_OUTPUT_KEY)
         },
     )
     _container_op.ContainerOp._DISABLE_REUSABLE_COMPONENT_WARNING = old_warn_value
 
     artifact_type_schema = type_utils.get_artifact_type_schema(artifact_class)
+    metadata_with_placeholders, metadata_inputs = transform_metadata_and_get_inputs(
+        metadata)
+    all_task_inputs = [input_uri_param] + metadata_inputs
+
     task.importer_spec = _build_importer_spec(
-        artifact_uri=artifact_uri, artifact_type_schema=artifact_type_schema, metadata=metadata)
+        artifact_uri=artifact_uri,
+        artifact_type_schema=artifact_type_schema,
+        metadata_with_placeholders=metadata_with_placeholders)
     task.task_spec = _build_importer_task_spec(
-        importer_base_name=task.name, artifact_uri=artifact_uri)
+        importer_base_name=task.name, inputs=all_task_inputs)
+
     task.component_spec = _build_importer_component_spec(
-        importer_base_name=task.name, artifact_type_schema=artifact_type_schema)
-    task.inputs = [input_param]
+        importer_base_name=task.name,
+        artifact_type_schema=artifact_type_schema,
+        inputs=all_task_inputs)
 
     return task
--- sdk/python/kfp/v2/components/importer_node_test.py
+++ sdk/python/kfp/v2/components/importer_node_test.py
@@ -12,12 +12,16 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import os
+import tempfile
 import unittest
 
+import yaml
 from absl.testing import parameterized
 from google.protobuf import json_format
 from kfp.dsl import _pipeline_param
 from kfp.pipeline_spec import pipeline_spec_pb2 as pb
+from kfp.v2 import compiler, dsl
 from kfp.v2.components import importer_node
 
 
@@ -30,7 +34,9 @@ class ImporterNodeTest(parameterized.TestCase):
                 'gs://artifact',
             'artifact_type_schema':
                 pb.ArtifactTypeSchema(schema_title='system.Dataset'),
-            'metadata': {'key':'value'},
+            'metadata': {
+                'key': 'value'
+            },
             'expected_result': {
                 'artifactUri': {
                     'constantValue': {
@@ -61,11 +67,14 @@ class ImporterNodeTest(parameterized.TestCase):
                 }
             },
         })
-    def test_build_importer_spec(self, input_uri, artifact_type_schema, metadata, expected_result):
+    def test_build_importer_spec(self, input_uri, artifact_type_schema,
+                                 metadata, expected_result):
         expected_importer_spec = pb.PipelineDeploymentConfig.ImporterSpec()
         json_format.ParseDict(expected_result, expected_importer_spec)
         importer_spec = importer_node._build_importer_spec(
-            artifact_uri=input_uri, artifact_type_schema=artifact_type_schema, metadata=metadata)
+            artifact_uri=input_uri,
+            artifact_type_schema=artifact_type_schema,
+            metadata_with_placeholders=metadata)
 
         self.maxDiff = None
         self.assertEqual(expected_importer_spec, importer_spec)
@@ -74,16 +83,15 @@ class ImporterNodeTest(parameterized.TestCase):
         {
             # artifact_uri is a constant value
             'importer_name': 'importer-1',
-            'input_uri': 'gs://artifact',
+            'inputs': [
+                _pipeline_param.PipelineParam(
+                    name='uri', value='gs://artifact', param_type='String')
+            ],
             'expected_result': {
                 'inputs': {
                     'parameters': {
                         'uri': {
-                            'runtimeValue': {
-                                'constantValue': {
-                                    'stringValue': 'gs://artifact'
-                                }
-                            }
+                            'componentInputParameter': 'uri'
                         }
                     }
                 },
@@ -95,11 +103,11 @@ class ImporterNodeTest(parameterized.TestCase):
         {
             # artifact_uri is from PipelineParam
             'importer_name': 'importer-2',
-            'input_uri': _pipeline_param.PipelineParam(name='uri_to_import'),
+            'inputs': [_pipeline_param.PipelineParam(name='uri_to_import')],
             'expected_result': {
                 'inputs': {
                     'parameters': {
-                        'uri': {
+                        'uri_to_import': {
                             'componentInputParameter': 'uri_to_import'
                         }
                     }
@@ -109,15 +117,17 @@ class ImporterNodeTest(parameterized.TestCase):
                 },
             },
         })
-    def test_build_importer_task_spec(self, importer_name, input_uri,
+    def test_build_importer_task_spec(self, importer_name, inputs,
                                       expected_result):
         expected_task_spec = pb.PipelineTaskSpec()
         json_format.ParseDict(expected_result, expected_task_spec)
 
         task_spec = importer_node._build_importer_task_spec(
-            importer_base_name=importer_name, artifact_uri=input_uri)
+            importer_base_name=importer_name, inputs=inputs)
 
         self.maxDiff = None
+        print(task_spec)
+        print(expected_task_spec)
         self.assertEqual(expected_task_spec, task_spec)
 
     def test_build_importer_component_spec(self):
@@ -146,7 +156,11 @@ class ImporterNodeTest(parameterized.TestCase):
         importer_comp_spec = importer_node._build_importer_component_spec(
             importer_base_name='importer-1',
             artifact_type_schema=pb.ArtifactTypeSchema(
-                schema_title='system.Artifact'))
+                schema_title='system.Artifact'),
+            inputs=[
+                _pipeline_param.PipelineParam(
+                    name='uri', op_name='func', value=None, param_type='String')
+            ])
 
         self.maxDiff = None
         self.assertEqual(expected_importer_comp_spec, importer_comp_spec)
@@ -158,6 +172,80 @@ class ImporterNodeTest(parameterized.TestCase):
                 "Importer got unexpected artifact_uri: 123 of type: <class 'int'>."
         ):
             importer_node.importer(artifact_uri=123, artifact_class=Dataset)
+
+    def test_dynamic_importer_metadata(self):
+
+        @dsl.component
+        def func() -> str:
+            return 'string'
+
+        @dsl.component
+        def func2() -> int:
+            return 1
+
+        @dsl.pipeline(name='pipeline')
+        def my_pipeline():
+            task = func()
+            task2 = func2()
+            dsl.importer(
+                artifact_uri=task.output,
+                artifact_class=dsl.Artifact,
+                metadata={
+                    task.output: task2.output,
+                    'other': [task.output, task2.output]
+                })
+
+        with tempfile.TemporaryDirectory() as tmpdir:
+            package_path = os.path.join(tmpdir, 'output.json')
+            compiler.Compiler().compile(my_pipeline, package_path=package_path)
+            with open(package_path) as f:
+                pipeline_spec_dict = yaml.safe_load(f.read())['pipelineSpec']
+
+        pipeline_spec = pb.PipelineSpec()
+        json_format.ParseDict(pipeline_spec_dict, pipeline_spec)
+
+        # test uri parameter
+        self.assertEqual(
+            pipeline_spec.deployment_spec['executors']['exec-importer']
+            ['importer']['artifactUri'].fields['runtimeParameter'].string_value,
+            'uri')
+
+        # test executor contents
+        self.assertEqual(
+            pipeline_spec.deployment_spec['executors']['exec-importer']
+            ['importer']['metadata']["{{$.inputs.parameters[\'Output-2\']}}"],
+            "{{$.inputs.parameters['Output']}}")
+        self.assertEqual(
+            pipeline_spec.deployment_spec['executors']['exec-importer']
+            ['importer']['metadata']["other"].values[0].string_value,
+            "{{$.inputs.parameters[\'Output-3\']}}")
+        self.assertEqual(
+            pipeline_spec.deployment_spec['executors']['exec-importer']
+            ['importer']['metadata']["other"].values[1].string_value,
+            "{{$.inputs.parameters[\'Output-4\']}}")
+
+        self.assertEqual(
+            pipeline_spec.components['comp-importer'].input_definitions
+            .parameters['Output'].type, 1)
+        self.assertEqual(
+            pipeline_spec.components['comp-importer'].input_definitions
+            .parameters['Output-2'].type, 3)
+        self.assertEqual(
+            pipeline_spec.components['comp-importer'].input_definitions
+            .parameters['Output-3'].type, 3)
+        self.assertEqual(
+            pipeline_spec.components['comp-importer'].input_definitions
+            .parameters['Output-4'].type, 1)
+        self.assertEqual(
+            pipeline_spec.components['comp-importer'].input_definitions
+            .parameters['uri'].type, 3)
+
+    def test_importer_with_none_metadata(self):
+        x = dsl.importer(
+            artifact_uri='', artifact_class=dsl.Artifact, metadata=None)
+        self.assertLen(x.component_spec.input_definitions.parameters, 1)
+        self.assertEqual(
+            x.component_spec.input_definitions.parameters['uri'].type, 3)
 
 
 if __name__ == '__main__':
