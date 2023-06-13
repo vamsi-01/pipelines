6d76ceab6140fde097323a83b3a8c8503788b1d2
connor-mccarthy <mccarthy.connor.james@gmail.com>
2023-06-12 17:01:26 -0700

    wip

--- f.py
+++ f.py
@@ -1,4 +1,4 @@
-PERSISTENT_RESOURCE_ID = 'test-persistent-resource'
+PERSISTENT_RESOURCE_ID = 'test-persistent-resource-no-accelerator'
 #
 import requests
 import subprocess
@@ -11,7 +11,7 @@ def get_access_token():
     return output.decode()
 
 
-PROJECT_NUMBER = 271009669852
+PROJECT_NUMBER = 186556260430
 PERSISTENT_RESOURCE_REQUEST = {
     "display_name":
         "Test-Persistent-Resource",
@@ -26,9 +26,7 @@ PERSISTENT_RESOURCE_REQUEST = {
         }
     }, {
         "machine_spec": {
-            "machine_type": "n1-standard-2",
-            "accelerator_type": "NVIDIA_TESLA_P4",
-            "accelerator_count": 2
+            "machine_type": "n1-standard-4",
         },
         "replica_count": 4,
         "disk_spec": {
@@ -48,48 +46,53 @@ response = requests.post(
         'Content-Type': 'application/json'
     })
 
-print(response.text)
-# from google_cloud_pipeline_components.v1 import custom_job
-# from kfp import dsl
-# from kfp.dsl import Artifact
-# from kfp.dsl import Input
-# from kfp.dsl import Output
-# import kfp
+# print(response.text)
+from google_cloud_pipeline_components.v1 import custom_job
+from kfp import dsl
+from kfp.dsl import Artifact
+from kfp.dsl import Input
+from kfp.dsl import Output
+import kfp
 
-# kfp.__version__ = '2.0.0-rc.1'
+kfp.__version__ = '2.0.0-rc.1'
 
-# @dsl.component
-# def sum_numbers(a: int = 1, b: int = 2) -> int:
-#     return a + b
 
-# comp = custom_job.create_custom_training_job_from_component(
-#     sum_numbers, persistent_resource_id=PERSISTENT_RESOURCE_ID)
+@dsl.component
+def sum_numbers(a: int = 1, b: int = 2) -> int:
+    return a + b
 
-# from kfp import dsl
 
-# @dsl.pipeline
-# def my_pipeline():
-#     comp(project='271009669852', location='us-central1')
+comp = custom_job.create_custom_training_job_from_component(
+    sum_numbers, persistent_resource_id=PERSISTENT_RESOURCE_ID)
 
-# if __name__ == '__main__':
-#     import datetime
-#     import warnings
-#     import webbrowser
+from kfp import dsl
 
-#     from google.cloud import aiplatform
 
-#     from kfp import compiler
+@dsl.pipeline
+def my_pipeline():
+    comp(project='271009669852', location='us-central1')
 
-#     warnings.filterwarnings('ignore')
-#     ir_file = __file__.replace('.py', '.yaml')
-#     compiler.Compiler().compile(pipeline_func=my_pipeline, package_path=ir_file)
-#     pipeline_name = __file__.split('/')[-1].replace('_', '-').replace('.py', '')
-#     display_name = datetime.datetime.now().strftime('%m-%d-%Y-%H-%M-%S')
-#     job_id = f'{pipeline_name}-{display_name}'
-#     aiplatform.PipelineJob(
-#         template_path=ir_file,
-#         pipeline_root='gs://cjmccarthy-kfp-default-bucket',
-#         display_name=pipeline_name,
-#         job_id=job_id).submit()
-#     url = f'https://console.cloud.google.com/vertex-ai/locations/us-central1/pipelines/runs/{pipeline_name}-{display_name}?project=271009669852'
-#     webbrowser.open_new_tab(url)
+
+if __name__ == '__main__':
+    import datetime
+    import warnings
+    import webbrowser
+
+    from google.cloud import aiplatform
+
+    from kfp import compiler
+
+    warnings.filterwarnings('ignore')
+    ir_file = __file__.replace('.py', '.yaml')
+    compiler.Compiler().compile(pipeline_func=my_pipeline, package_path=ir_file)
+    pipeline_name = __file__.split('/')[-1].replace('_', '-').replace('.py', '')
+    display_name = datetime.datetime.now().strftime('%m-%d-%Y-%H-%M-%S')
+    job_id = f'{pipeline_name}-{display_name}'
+    aiplatform.PipelineJob(
+        project='186556260430',
+        template_path=ir_file,
+        pipeline_root='gs://cjmccarthy-kfp-default-bucket',
+        display_name=pipeline_name,
+        job_id=job_id).submit()
+    url = f'https://console.cloud.google.com/vertex-ai/locations/us-central1/pipelines/runs/{pipeline_name}-{display_name}?project=186556260430'
+    webbrowser.open_new_tab(url)
